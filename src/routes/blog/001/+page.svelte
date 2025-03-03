50ecfa1a0d4737a895a77544c50363289a020be4
Simon Tinhofer <tinhofer.simon@gmail.com>
2025-03-03 14:44:51 +0100

    sdf

--- src/routes/blog/001/+page.svelte
+++ src/routes/blog/001/+page.svelte
@@ -1,82 +1,184 @@
 <script>
-    import thumbnail from '$lib/images/blog1.webp';
+	import thumbnail from '$lib/images/blog1.webp';
+	import ExternalLink from '$lib/ExternalLink.svelte';
 </script>
+
 <div class="blogBackground">
-    <div class="blogContainer">
-        <img src="{thumbnail}" alt="Bild von KI-Schulung">
-      <h1>Von der Theorie zur Praxis: KI-Schulungen für Mitarbeitende</h1>
-    
-      <p><strong>Seit 2. Februar 2025</strong> müssen Arbeitnehmer:innen für die Verwendung von ChatGPT & Co über KI-Kompetenz verfügen.</p>
-    
-      <p>Die Verordnung über künstliche Intelligenz (EU) 2024/1689 vom 13. Juni 2024 („KI-VO“) trat am 1. August 2024 in Kraft. Die meisten Bestimmungen werden erst ab 2. August 2026 anwendbar sein. <strong>Art 4 zur sog „KI-Kompetenz“</strong> findet jedoch bereits seit 2. Februar 2025 Anwendung und ist eine der wenigen Bestimmungen, die für alle KI-Systeme gelten. Es ist also keineswegs verfrüht, sich mit dieser neuen, unmittelbar anwendbaren Rechtsvorschrift auseinanderzusetzen.</p>
-    
-      <p>Die EU verfolgt mit Art 4 KI-VO das Ziel, die Chancen von KI-Systemen zu nutzen und gleichzeitig die Risiken für Grundrechte, Gesundheit und Sicherheit sowie Demokratie zu minimieren. Schließlich soll damit auch die Grundlage dafür geschaffen werden, dass die KI-VO von den jeweiligen Akteuren eingehalten werden kann.</p>
-    
-      <p>Im Folgenden wird dargelegt, welche Verpflichtungen sich aus Art 4 für Unternehmen ergeben, die KI-Systeme entwickeln oder (auch) betreiben, und wie diese erfüllt werden können.</p>
-    
-      <h2>Was bedeutet KI-Kompetenz?</h2>
-    
-      <p>Art 4 verpflichtet Anbieter:innen und Betreiber:innen von „KI-Systemen“ sicherzustellen, dass ihr Personal und andere beauftragte Personen über „ein ausreichendes Maß an KI-Kompetenz“ verfügen, wenn sie mit dem Betrieb und der Nutzung von KI-Systemen befasst sind.</p>
-    
-      <p>Zunächst ist also zu klären, was die KI-VO unter einem KI-System versteht. Dabei handelt es sich um einen zentralen Begriff für die Anwendung der Verordnung. Nach Art 3 Z 1 muss es sich um ein maschinengestütztes System handeln, das mit einem bestimmten Grad von Autonomie ausgestattet ist und – aufgrund expliziter oder impliziter Ziele – aus den Eingaben Vorhersagen, Empfehlungen, Entscheidungen oder andere Ausgabedaten erstellt, welche die physische oder virtuelle Umgebung beeinflussen können.</p>
-    
-      <p>In der Praxis werden die meisten Unternehmen die Rolle des Betreibers einnehmen. Darunter ist jede natürliche oder juristische Person, Behörde, Einrichtung oder sonstige Stelle zu verstehen, die ein KI-System in eigener Verantwortung verwendet (Art 3 Z 4). Wenn ein Unternehmen allerdings ein KI-System am Markt kauft und für eigene Zwecke weiterentwickelt, kann es auch zum Anbieter werden (Art 3 Z 3). Ein Unternehmen kann auch beide Rollen zugleich einnehmen.</p>
-    
-      <p>Der Begriff „KI-Kompetenz“ wird in Art 1 Z 56 wie folgt definiert:</p>
-    
-      <blockquote>
-          „… die Fähigkeiten, die Kenntnisse und das Verständnis, die es Anbietern, Betreibern und Betroffenen unter Berücksichtigung ihrer jeweiligen Rechte und Pflichten im Rahmen dieser Verordnung ermöglichen, KI-Systeme sachkundig einzusetzen sowie sich der Chancen und Risiken von KI und möglicher Schäden, die sie verursachen kann, bewusst zu werden.“
-      </blockquote>
-    
-      <p>Bei der Ausarbeitung eines konkreten Konzepts zur Vermittlung von KI-Kompetenz ist neben den technischen Kenntnissen, Erfahrungen und der Ausbildung der Mitarbeitenden bzw. beauftragten Personen insbesondere der Kontext, einschließlich der betroffenen Personengruppen, zu berücksichtigen, in welchem die KI-Systeme eingesetzt werden sollen (Art 4). Schulungsmaßnahmen sind also personen- und funktionsspezifisch zu konzipieren.</p>
-    
-      <p>Es gibt daher kein allgemein gültiges Schulungskonzept, das Art 4 in jedem Fall genügen würde. Allerdings sollten Schulungsmaßnahmen grundsätzlich zumindest folgende Fragen behandeln:</p>
-    
-      <ul>
-          <li>Wie funktionieren die verwendeten KI-Systeme? Was sind ihre Stärken und Schwächen?</li>
-          <li>Wofür sollen die KI-Systeme eingesetzt werden („Use Cases“)?</li>
-          <li>Welche rechtlichen Vorgaben sind dabei zu beachten?</li>
-      </ul>
-    
-      <p>In didaktischer Hinsicht stehen neben dem klassischen Frontalunterricht zahlreiche andere Formate zur Verfügung, die für die Vermittlung anwendungsorientierten Wissens im Rahmen einer betrieblichen Fortbildung besonders geeignet erscheinen. So bieten sich neben Workshops auch Konzepte des Blended Learning (Kombination von Präsenzveranstaltungen und E-Learning), des Microlearning (Vermittlung von Wissen in kleinen, leicht verdaulichen Einheiten) oder der Gamification (spielerische Elemente wie Quiz oder Wettbewerbe zur Förderung der Motivation) an.</p>
-    
-      <h2>Was können Unternehmen jetzt tun?</h2>
-    
-      <p>Um den Anforderungen des Art 4 KI-VO an die KI-Kompetenz ihres Personals gerecht zu werden, sollten Unternehmen ehestmöglich tätig werden. Sie könnten dabei wie folgt vorgehen:</p>
-    
-      <ol>
-          <li>
-              <p>In einem ersten Schritt werden im Rahmen einer Kompetenzanalyse die vorhandenen Kompetenzen systematisch erfasst und Kompetenzlücken identifiziert. Im Hinblick auf die sehr dynamische Entwicklung von KI-Anwendungen sollte dieser Schritt in bestimmten zeitlichen Abständen regelmäßig wiederholt werden.</p>
-          </li>
-          <li>
-              <p>Im zweiten Schritt werden konkrete Schulungskonzepte erstellt, die auf die unterschiedlichen Bedürfnisse der Mitarbeitenden im Zusammenhang mit den von ihnen verwendeten KI-Systemen abgestimmt werden müssen. Dabei könnten auch KI-gestützte Tools eingesetzt werden, durch die insbesondere eine Individualisierung der Lerninhalte und -methoden erreicht werden kann.</p>
-          </li>
-          <li>
-              <p>Nicht vergessen werden sollte dabei die Dokumentation all dieser Aktivitäten, damit diese gegenüber Dritten (z. B. einer Behörde) bei Bedarf nachgewiesen werden können.</p>
-          </li>
-          <li>
-              <p>Schließlich sind die getroffenen Maßnahmen regelmäßig auf ihre Wirksamkeit zu prüfen.</p>
-          </li>
-      </ol>
-    
-      <p><strong>Dr. Andreas Tinhofer, LL.M.</strong>, ist Rechtsanwalt in der Arbeitsrechtskanzlei Krichmayr Tinhofer. Sein besonderes Interesse gilt dem Einsatz von KI in der Arbeitswelt.</p>
-    </div>
+	<div class="blogContainer">
+		<img src={thumbnail} alt="Bild von KI-Schulung" />
+		<h1>Von der Theorie zur Praxis: KI-Schulungen für Mitarbeitende</h1>
+
+		<p>
+			<strong>Seit 2. Februar 2025</strong> müssen Arbeitnehmer:innen für die Verwendung von ChatGPT
+			& Co über KI-Kompetenz verfügen.
+		</p>
+
+		<p>
+			Die Verordnung über künstliche Intelligenz (EU) 2024/1689 vom 13. Juni 2024 („KI-VO“) trat am
+			1. August 2024 in Kraft. Die meisten Bestimmungen werden erst ab 2. August 2026 anwendbar
+			sein. <strong>Art 4 zur sog „KI-Kompetenz“</strong> findet jedoch bereits seit 2. Februar 2025
+			Anwendung und ist eine der wenigen Bestimmungen, die für alle KI-Systeme gelten. Es ist also keineswegs
+			verfrüht, sich mit dieser neuen, unmittelbar anwendbaren Rechtsvorschrift auseinanderzusetzen.
+		</p>
+
+		<p>
+			Die EU verfolgt mit Art 4 KI-VO das Ziel, die Chancen von KI-Systemen zu nutzen und
+			gleichzeitig die Risiken für Grundrechte, Gesundheit und Sicherheit sowie Demokratie zu
+			minimieren. Schließlich soll damit auch die Grundlage dafür geschaffen werden, dass die KI-VO
+			von den jeweiligen Akteuren eingehalten werden kann.
+		</p>
+
+		<p>
+			Im Folgenden wird dargelegt, welche Verpflichtungen sich aus Art 4 für Unternehmen ergeben,
+			die KI-Systeme entwickeln oder (auch) betreiben, und wie diese erfüllt werden können.
+		</p>
+
+		<h2>Was bedeutet KI-Kompetenz?</h2>
+
+		<p>
+			Art 4 verpflichtet Anbieter:innen und Betreiber:innen von „KI-Systemen“ sicherzustellen, dass
+			ihr Personal und andere beauftragte Personen über „ein ausreichendes Maß an KI-Kompetenz“
+			verfügen, wenn sie mit dem Betrieb und der Nutzung von KI-Systemen befasst sind.
+		</p>
+
+		<p>
+			Zunächst ist also zu klären, was die KI-VO unter einem KI-System versteht. Dabei handelt es
+			sich um einen zentralen Begriff für die Anwendung der Verordnung. Nach Art 3 Z 1 muss es sich
+			um ein maschinengestütztes System handeln, das mit einem bestimmten Grad von Autonomie
+			ausgestattet ist und – aufgrund expliziter oder impliziter Ziele – aus den Eingaben
+			Vorhersagen, Empfehlungen, Entscheidungen oder andere Ausgabedaten erstellt, welche die
+			physische oder virtuelle Umgebung beeinflussen können.
+		</p>
+
+		<p>
+			In der Praxis werden die meisten Unternehmen die Rolle des Betreibers einnehmen. Darunter ist
+			jede natürliche oder juristische Person, Behörde, Einrichtung oder sonstige Stelle zu
+			verstehen, die ein KI-System in eigener Verantwortung verwendet (Art 3 Z 4). Wenn ein
+			Unternehmen allerdings ein KI-System am Markt kauft und für eigene Zwecke weiterentwickelt,
+			kann es auch zum Anbieter werden (Art 3 Z 3). Ein Unternehmen kann auch beide Rollen zugleich
+			einnehmen.
+		</p>
+
+		<p>Der Begriff „KI-Kompetenz“ wird in Art 1 Z 56 wie folgt definiert:</p>
+
+		<blockquote>
+			„… die Fähigkeiten, die Kenntnisse und das Verständnis, die es Anbietern, Betreibern und
+			Betroffenen unter Berücksichtigung ihrer jeweiligen Rechte und Pflichten im Rahmen dieser
+			Verordnung ermöglichen, KI-Systeme sachkundig einzusetzen sowie sich der Chancen und Risiken
+			von KI und möglicher Schäden, die sie verursachen kann, bewusst zu werden.“
+		</blockquote>
+
+		<p>
+			Bei der Ausarbeitung eines konkreten Konzepts zur Vermittlung von KI-Kompetenz ist neben den
+			technischen Kenntnissen, Erfahrungen und der Ausbildung der Mitarbeitenden bzw. beauftragten
+			Personen insbesondere der Kontext, einschließlich der betroffenen Personengruppen, zu
+			berücksichtigen, in welchem die KI-Systeme eingesetzt werden sollen (Art 4).
+			Schulungsmaßnahmen sind also personen- und funktionsspezifisch zu konzipieren.
+		</p>
+
+		<p>
+			Es gibt daher kein allgemein gültiges Schulungskonzept, das Art 4 in jedem Fall genügen würde.
+			Allerdings sollten Schulungsmaßnahmen grundsätzlich zumindest folgende Fragen behandeln:
+		</p>
+
+		<ul>
+			<li>Wie funktionieren die verwendeten KI-Systeme? Was sind ihre Stärken und Schwächen?</li>
+			<li>Wofür sollen die KI-Systeme eingesetzt werden („Use Cases“)?</li>
+			<li>Welche rechtlichen Vorgaben sind dabei zu beachten?</li>
+		</ul>
+
+		<p>
+			In didaktischer Hinsicht stehen neben dem klassischen Frontalunterricht zahlreiche andere
+			Formate zur Verfügung, die für die Vermittlung anwendungsorientierten Wissens im Rahmen einer
+			betrieblichen Fortbildung besonders geeignet erscheinen. So bieten sich neben Workshops auch
+			Konzepte des Blended Learning (Kombination von Präsenzveranstaltungen und E-Learning), des
+			Microlearning (Vermittlung von Wissen in kleinen, leicht verdaulichen Einheiten) oder der
+			Gamification (spielerische Elemente wie Quiz oder Wettbewerbe zur Förderung der Motivation)
+			an.
+		</p>
+
+		<h2>Was können Unternehmen jetzt tun?</h2>
+
+		<p>
+			Um den Anforderungen des Art 4 KI-VO an die KI-Kompetenz ihres Personals gerecht zu werden,
+			sollten Unternehmen ehestmöglich tätig werden. Sie könnten dabei wie folgt vorgehen:
+		</p>
+
+		<ol>
+			<li>
+				<p>
+					In einem ersten Schritt werden im Rahmen einer Kompetenzanalyse die vorhandenen
+					Kompetenzen systematisch erfasst und Kompetenzlücken identifiziert. Im Hinblick auf die
+					sehr dynamische Entwicklung von KI-Anwendungen sollte dieser Schritt in bestimmten
+					zeitlichen Abständen regelmäßig wiederholt werden.
+				</p>
+			</li>
+			<li>
+				<p>
+					Im zweiten Schritt werden konkrete Schulungskonzepte erstellt, die auf die
+					unterschiedlichen Bedürfnisse der Mitarbeitenden im Zusammenhang mit den von ihnen
+					verwendeten KI-Systemen abgestimmt werden müssen. Dabei könnten auch KI-gestützte Tools
+					eingesetzt werden, durch die insbesondere eine Individualisierung der Lerninhalte und
+					-methoden erreicht werden kann.
+				</p>
+			</li>
+			<li>
+				<p>
+					Nicht vergessen werden sollte dabei die Dokumentation all dieser Aktivitäten, damit diese
+					gegenüber Dritten (z. B. einer Behörde) bei Bedarf nachgewiesen werden können.
+				</p>
+			</li>
+			<li>
+				<p>Schließlich sind die getroffenen Maßnahmen regelmäßig auf ihre Wirksamkeit zu prüfen.</p>
+			</li>
+		</ol>
+
+		<p>
+			<strong>Dr. Andreas Tinhofer, LL.M.</strong>, ist Rechtsanwalt in der Arbeitsrechtskanzlei
+			Krichmayr Tinhofer. Sein besonderes Interesse gilt dem Einsatz von KI in der Arbeitswelt.
+		</p>
+
+		Weitere Ressourcen:
+		<ul>
+			<li>
+				<a
+					href="https://digital-strategy.ec.europa.eu/de/library/living-repository-foster-learning-and-exchange-ai-literacy"
+					>living-repository-foster-learning-and-exchange-ai-literacy<ExternalLink /></a
+				>
+			</li>
+			<li>
+				<a href="https://digital-strategy.ec.europa.eu/en/events/third-ai-pact-webinar-ai-literacy"
+					>third-ai-pact-webinar-ai-literacy<ExternalLink /></a
+				>
+			</li>
+		</ul>
+	</div>
 </div>
 
 <style>
-    img{
-        width: 100%;
-    }
-    .blogBackground{
-        background-color: var(--white);
-        padding: 1rem;
-    }
-    .blogContainer{
-        background-color: var(--white);
-        padding: 2rem;
-        max-width: 70ch;
-        margin:auto;
-        font-size: var(--fs-p);
-    }
+	img {
+		width: 100%;
+	}
+	.blogBackground {
+		background-color: var(--white);
+		padding: 1rem;
+	}
+	.blogContainer {
+		background-color: var(--white);
+		padding: 2rem;
+		max-width: 70ch;
+		margin: auto;
+		font-size: var(--fs-p);
+	}
+	a {
+		white-space: nowrap;
+		color: var(--cyan);
+		text-decoration: none;
+	}
+	a:hover {
+		background-image: linear-gradient(to bottom, currentcolor, 75%, transparent 75%);
+		background-position: 0 1.1em;
+		background-repeat: repeat-x;
+		background-size: 5em 0.15em;
+	}
 </style>
-
